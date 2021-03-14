a816edbc6a3535a74c22974458bcaa82f7c76702
Daniel Barter <danielbarter@gmail.com>
2021-03-14 09:21:38 -0700

    more extensive test

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 from ArtinWedderburn import *
-alg = symmetric_group_algebra(3)
 svd_threshold = 0.00001
-aw = ArtinWedderburn(alg,svd_threshold)
+
+
+print("irreps for S2")
+ArtinWedderburn(symmetric_group_algebra(2),svd_threshold)
+print("\n\n\n")
+
+print("irreps for S3")
+ArtinWedderburn(symmetric_group_algebra(3),svd_threshold)
+print("\n\n\n")
+
+
+print("irreps for S4")
+ArtinWedderburn(symmetric_group_algebra(4),svd_threshold)
