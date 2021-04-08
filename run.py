874135dce2c9c7931182b9ed3b4f82f31f605da3
Daniel Barter <danielbarter@gmail.com>
2021-04-07 19:31:08 -0700

    profiling

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 import sys
 import os
 from ArtinWedderburn import *
+import cProfile, pstats, io
+from pstats import SortKey
 
 
 if len(sys.argv) != 2:
     print("usage: python run.py algebra_file")
     exit()
 
+with cProfile.Profile() as pr:
+    alg = load_algebra_from_file(sys.argv[1])
+    aw = ArtinWedderburn(alg, logging=True)
 
-from ArtinWedderburn import *
-alg = load_algebra_from_file(sys.argv[1])
-aw = ArtinWedderburn(alg, logging=True)
+
+s = io.StringIO()
+sortby = SortKey.CUMULATIVE
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print(s.getvalue())
