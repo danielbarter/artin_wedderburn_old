176c2e5794206f3d29fba4c075f47f50adbd9c86
Daniel Barter <danielbarter@gmail.com>
2021-04-08 16:27:37 -0700

    cleaning up nix shell

--- shell.nix
+++ shell.nix
@@ -3,18 +3,11 @@ with (import <nixpkgs> {});
 let pythonEnv = python38.withPackages (
       ps: [ ps.numpy
             ps.scipy
-            ps.cupy
           ]);
 
 in mkShell rec {
 
   buildInputs = [ pythonEnv
-                  nvtop
                 ];
-
-  # nvtop can't find shared libraries. Should be fixed in nvtop
-  shellHook = ''
-    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.linuxPackages.nvidia_x11}/lib
-   '';
 
 }
